fcc5f5edd362d370d78e98ac830e189f998722e5
LNqht <103646808+LNqht@users.noreply.github.com>
2022-05-26 19:39:39 +0800

    week 13

--- src/main/java/com/qiuhongtao/controller/UserListServlet.java
+++ src/main/java/com/qiuhongtao/controller/UserListServlet.java
@@ -22,11 +22,11 @@ public class UserListServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String path="../WEB-INF/views/admin/userList.jsp";
+        request.getRequestDispatcher(path).forward(request,response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String path="../WEB-INF/views/admin/userList.jsp";
-        request.getRequestDispatcher(path).forward(request,response);
     }
 }
